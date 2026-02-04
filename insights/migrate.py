19e6d9111abb78bab7991f3910ceaf3d48cf954a
Saqib Ansari <thefalconx33@gmail.com>
2026-02-04 13:00:24 +0530

    revert: "fix: add before_migrate function to restore site db credentials" (#765)

--- insights/migrate.py
+++ insights/migrate.py
@@ -4,36 +4,12 @@
 
 import frappe
 
-_creds = None
-
-
-def before_migrate():
-    global _creds
-    if frappe.db.exists("Insights Data Source v3", "Site DB"):
-        _creds = frappe.db.get_value(
-            "Insights Data Source v3",
-            "Site DB",
-            [
-                "host",
-                "port",
-                "username",
-                "password",
-                "database_name",
-                "use_ssl",
-                "enable_stored_procedure_execution",
-            ],
-            as_dict=True,
-        )
-
 
 def after_migrate():
-    global _creds
     try:
         create_admin_team()
-        if _creds:
-            frappe.db.set_value("Insights Data Source v3", "Site DB", _creds)
     except Exception:
-        frappe.log_error(title="Error in after_migrate")
+        frappe.log_error(title="Error creating Admin Team")
 
 
 def create_admin_team():
@@ -45,3 +21,4 @@ def create_admin_team():
                 "team_members": [{"user": "Administrator"}],
             }
         ).insert(ignore_permissions=True)
+
